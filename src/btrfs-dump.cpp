590b304e8a79998baea5ea86feddc5b27c7ffa77
Mark Harmstone <mark@harmstone.com>
2025-09-11 10:27:26 -0400

    open file

--- src/btrfs-dump.cpp
+++ src/btrfs-dump.cpp
@@ -1,4 +1,27 @@
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+
 import cxxbtrfs;
+import formatted_error;
+
+using namespace std;
+
+static void dump(const filesystem::path& fn) {
+    ifstream f(fn);
+
+    if (f.fail())
+        throw formatted_error("Failed to open {}", fn.string()); // FIXME - include why
+
+    // FIXME
+}
 
 int main() {
+    // FIXME - solicit filename
+
+    try {
+        dump("test");
+    } catch (const exception& e) {
+        cerr << "Exception: " << e.what() << endl;
+    }
 }
